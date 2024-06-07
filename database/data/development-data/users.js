6b9ddaed216d75d93ef0e25e45dc09376640c7aa
Howling Techie <howling.techie@gmail.com>
2024-06-07 14:49:52 +0100

    Replaced user passwords with hashed versions

--- database/data/development-data/users.js
+++ database/data/development-data/users.js
@@ -4,7 +4,8 @@ module.exports = [
         "username": "john_doe",
         "display_name": "John Doe",
         "email": "john.doe@example.com",
-        "password": "password123",
+        "password": "$2b$10$j6SAl7Iv0yyC/c6JPXA0vOlHZ1f3Zs0H8gG1luC48wgR/1Bw3NV1q",
+        // password123
         "about": "A software developer from New York."
     },
     {
@@ -12,7 +13,8 @@ module.exports = [
         "username": "jane_smith",
         "display_name": "Jane Smith",
         "email": "jane.smith@example.com",
-        "password": "password456",
+        "password": "$2b$10$JENuMQApZQH5F6rSOOIXbeMBpt5RAUj1uALktv7mmweDVxstZnoVS",
+        // password456
         "about": "A graphic designer from California."
     },
     {
@@ -20,7 +22,8 @@ module.exports = [
         "username": "mike_jones",
         "display_name": "Mike Jones",
         "email": "mike.jones@example.com",
-        "password": "password789",
+        "password": "$2b$10$ghO3ObJVbeFL9tK3uFY7GORr9S/9RhKP0.4UoWNRArnXdP8GYoGh6",
+        // password789
         "about": "A project manager from Texas."
     },
     {
@@ -28,7 +31,8 @@ module.exports = [
         "username": "emily_white",
         "display_name": "Emily White",
         "email": "emily.white@example.com",
-        "password": "password101",
+        "password": "$2b$10$g38Kdw7yS.EkzslNNhuSceBRjN6zVsJPhv/WjIwwt14ZROK3EN.ZS",
+        // password101
         "about": "A content writer from Florida."
     },
     {
@@ -36,7 +40,8 @@ module.exports = [
         "username": "david_brown",
         "display_name": "David Brown",
         "email": "david.brown@example.com",
-        "password": "password202",
+        "password": "$2b$10$SA34DQ.8jK4UFxJCpkq.vOzzqGi6uUB18xuHK7oh3oVCfxryPj..K",
+        // password202
         "about": "A data analyst from Illinois."
     }
 ];

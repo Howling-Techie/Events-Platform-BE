25dfac6e3afedbdd0ab8e0eb61f53cd93e5b5a1a
Howling Techie <howling.techie@gmail.com>
2024-06-10 16:44:31 +0100

    Added access levels to usergroup seed data.

--- database/data/development-data/userGroups.js
+++ database/data/development-data/userGroups.js
@@ -1,30 +1,37 @@
 module.exports = [
     {
         "user_id": 1,
-        "group_id": 1
+        "group_id": 1,
+        "access_level": 3
     },
     {
         "user_id": 2,
-        "group_id": 2
+        "group_id": 2,
+        "access_level": 3
     },
     {
         "user_id": 3,
-        "group_id": 3
+        "group_id": 3,
+        "access_level": 3
     },
     {
         "user_id": 4,
-        "group_id": 4
+        "group_id": 4,
+        "access_level": 3
     },
     {
         "user_id": 5,
-        "group_id": 5
+        "group_id": 5,
+        "access_level": 3
     },
     {
         "user_id": 1,
-        "group_id": 2
+        "group_id": 2,
+        "access_level": 1
     },
     {
         "user_id": 2,
-        "group_id": 3
+        "group_id": 3,
+        "access_level": 1
     }
 ];
